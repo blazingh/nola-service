eab202051eb89fbae2a37b4a22d337df2accaf1b
hadibabaki <hadibaalbaki2@gmail.com>
2023-07-23 15:03:22 +0300

    added database seed file

---
+++ src/database/seed.sql
@@ -0,0 +1,11 @@
+insert into setting_entity (name, value, description)
+values 
+('DISABLE_EMAIL_LOGIN', '0', 'Disable email login'),
+('DISABLE_PHONE_LOGIN', '0', 'Disable phone login'),
+('DISABLE_EMAIL_SIGNUP', '0', 'Disable email signup'),
+('DISABLE_PHONE_SIGNUP', '0', 'Disable phone signup'),
+('ALLOW_INACTIVE_USER_LOGIN', '0', 'Allow inactive user login'),
+('ALLOW_UNVERIFIED_PHONE_LOGIN', '0', 'Allow unverified phone login'),
+('ALLOW_UNVERIFIED_EMAIL_LOGIN', '0', 'Allow unverified email login'),
+('ALLOW_EMAIL_TWO_FACTOR_AUTH', '0', 'Allow email two factor authentication'),
+('ALLOW_PHONE_TWO_FACTOR_AUTH', '0', 'Allow phone two factor authentication');
